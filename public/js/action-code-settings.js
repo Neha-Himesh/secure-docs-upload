5cb27d85e86ad94408fb855e09a78f650f6ace69
Neha-Himesh <neha.himesh@spike.sh>
2025-04-18 00:28:21 +0530

    Uncommented verify-email page

--- public/js/action-code-settings.js
+++ public/js/action-code-settings.js
@@ -1,7 +1,7 @@
 export const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://upload-25f01.web.app/', //finishSignUp
+    url: 'https://upload-25f01.web.app/verify-email', //finishSignUp
     // This must be true.
     handleCodeInApp: true,
     iOS: {
